8595fe4621bb6a75096608c62b666f4c9f5eab8a
sahil-shah0203 <sahil0203@utexas.edu>
2024-05-20 02:33:24 -0500

    db created, classes being added, now needs storage for class to user connection

--- src/trpc/router/createRouter.ts
+++
@@ -1,3 +0,0 @@
-import { router } from '@trpc/server';
-
-export const createRouter = () => router();
--- src/trpc/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter } from '~/trpc/createRouter';
-import { userRouter } from './user';
-import { classRouter } from './class';
-
-export const appRouter = createRouter()
-  .merge('user.', userRouter)
-  .merge('class.', classRouter);
-
-export type AppRouter = typeof appRouter;
