84d1b2ea172eae2a5d2cc8f2372095abb0096771
Haris Sattar <harissattar8@gmail.com>
2024-05-30 14:09:39 -0500

    removing code that doesnt work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,11 @@
 import { useUser } from '@clerk/nextjs';
 import Landing from './landing';
 import Home from './home'; 
-import { useEffect } from 'react';
 import Sidebar from './_components/Sidebar';
 
 export default function MainPage() {
   const { isSignedIn } = useUser();
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
 
   if (!isSignedIn) {
     return <Landing />;
@@ -24,40 +20,4 @@ export default function MainPage() {
       </div>
     </div>
   );
-}
-
-interface Post {
-  id: number;
-  name: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-interface GetAllResponse {
-  result: {
-    data: {
-      json: Post[];
-    };
-  };
-}
-
-async function fetchPosts() {
-  try {
-    const response = await fetch('/api/trpc/post.getAll', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      console.error('Failed to fetch:', response.statusText);
-      return;
-    }
-
-    const data: GetAllResponse = await response.json();
-    console.log(data.result.data.json[0]?.id);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
 }
