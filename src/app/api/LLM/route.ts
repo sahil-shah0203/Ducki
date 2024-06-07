9d2de4b819540f632c81ba25c87157998b94c513
sahil-shah0203 <sahil0203@utexas.edu>
2024-06-07 16:36:08 -0500

    switching gpt to 4o

--- src/app/api/LLM/route.ts
+++ src/app/api/LLM/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
 
     // Sending to ChatGPT
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-4o",
       messages: [
         {
           role: "system",
