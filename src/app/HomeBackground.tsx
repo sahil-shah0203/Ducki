6047aa57698fabde2771d7cae5c552286856d40d
yangmichaely <yangmichaely@gmail.com>
2024-07-01 22:28:47 -0500

    trying to fix the white background

--- src/app/HomeBackground.tsx
+++ src/app/HomeBackground.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '~/styles/globals.css';
 
 const HomeBackground: React.FC = () => {
   return (
-    <div className="home-background-outer">
-      <div className="home-background-inner" />
+    <div className="home-background-wrapper">
+      <div className="home-background-container">
+        <div className="home-background-inner" />
+      </div>
     </div>
   );
 };
