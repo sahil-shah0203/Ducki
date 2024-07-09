5dd2dfeaf649930681a429c7273c28d266808a2b
yangmichaely <yangmichaely@gmail.com>
2024-07-09 12:09:15 -0500

    fixing build issues

--- src/app/_components/sidebar_components/calendar.tsx
+++ src/app/_components/sidebar_components/calendar.tsx
@@ -4,25 +4,11 @@ import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { fetchCalendarEvents } from '~/app/services/calendarService';
 
 const localizer = momentLocalizer(moment);
 
 const MyCalendar: React.FC = () => {
   const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const getEvents = async () => {
-      try {
-        const data = await fetchCalendarEvents();
-        setEvents(data);
-      } catch (error) {
-        console.error('Failed to fetch events:', error);
-      }
-    };
-
-    getEvents();
-  }, []);
 
   return (
     <div style={{ height: '500pt', width: '100%'}}>
--- src/app/services/calendarService.ts
+++
@@ -1,5 +0,0 @@
-export const fetchCalendarEvents = async () => {
-  const response = await fetch('~/api/Calendar');
-  const data = await response.json();
-  return data;
-};
